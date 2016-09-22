83a47ccb59654f0da10886e8f6c88375fe8ea8f4
Quinn Maurmann <quinn.maurmann@gmail.com>
2016-09-22 15:37:57 -0700

    Don't get mongodb from database-cleaner/node_modules

--- db.cleaner.js
+++ db.cleaner.js
@@ -1,6 +1,11 @@
 var DatabaseCleaner = require('database-cleaner');
 var cleaner = new DatabaseCleaner('mongodb');
-var connect = require('database-cleaner/node_modules/mongodb').connect;
+
+// TODO getting mongodb from (peer dep) mongoose/node_modules is not great
+// because (a) the path is an implementation detail (though one unlikely to
+// change) and (b) apps should be able to use this repo without using mongoose.
+// Consider injecting mongodb object or connect function.
+var connect = require('mongoose/node_modules/mongodb').connect;
 
 module.exports = function (url) { return function (callback) {
   connect(url, function (error, db) {
