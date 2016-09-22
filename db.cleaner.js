c83d0a18dc52b52f735b1221ad4a44c9ea16cd71
Quinn Maurmann <quinn.maurmann@gmail.com>
2016-09-22 15:42:35 -0700

    Didn't work... just inject connect

--- db.cleaner.js
+++ db.cleaner.js
@@ -1,13 +1,9 @@
 var DatabaseCleaner = require('database-cleaner');
 var cleaner = new DatabaseCleaner('mongodb');
 
-// TODO getting mongodb from (peer dep) mongoose/node_modules is not great
-// because (a) the path is an implementation detail (though one unlikely to
-// change) and (b) apps should be able to use this repo without using mongoose.
-// Consider injecting mongodb object or connect function.
-var connect = require('mongoose/node_modules/mongodb').connect;
-
-module.exports = function (url) { return function (callback) {
+// Inject require('mongodb').connect function to match the version of mongodb
+// you use.
+module.exports = function (connect, url) { return function (callback) {
   connect(url, function (error, db) {
     cleaner.clean(db, function () {
       db.close();
